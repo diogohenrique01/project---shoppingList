3e3806261be6ec1667d58ca3bf66bd21c4fd5ae9
Diogo Henrique <dioghohenrique@gmail.com>
2025-06-10 11:06:20 -0300

    obtendo o item selecionado

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const input = document.getElementById("new-item")
+const form = document.querySelector("form")
+const items = document.querySelector("items")
+const list = document.querySelector(".list")
+
+
+//substituindo números por str vazias
+input.addEventListener("input", (event)=> {
+  const hasCharactersRegex = /\d+/g
+  input.value = input.value.replace(hasCharactersRegex, "") 
+})
+
+//novo valor, que continua após os 4
+let itemId = 5 
+
+//obtento o item selecionado
+form.addEventListener("submit", (event)=>{
+  event.preventDefault()
+  const itemText = input.value.trim()
+  if (itemText === "") return
+
+  const li = document.createElement("li")
+  li.className = "items"
+  li.innerHTML = `
+    <input type="checkbox" id="item${itemText}" class="checkbox" />
+    <label for="item${itemText}" class="item">${itemText}</label>
+    <button class="icon-delete" aria-label="Remover item">
+    <img class="frame" src="https://c.animaapp.com/mbi0tglf1RxiZ6/img/frame.svg" alt="Ícone de lixeira" />
+    </button>
+  `
+  list.appendChild(li)
+  itemId++
+  input.value = ""
+
+})
+
+
+
